388336a4ace75725de6bd8e114850ea4e1589b5d
Ebne Samit Chowdhury <ebnesm2@gmail.com>
2026-01-09 19:46:10 +0600

    jan8,26

---
+++ A_Meximization.cpp
@@ -0,0 +1,62 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ //
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define opscode()                     \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+#define dbg(p) cerr << #p << " " << p << "\n";
+#else
+#define dbg(p)
+#endif
+
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+typedef long long ll;
+typedef double dol;
+#define pi acos(-1)
+#define int long long
+
+void samsolveit()
+{
+    int nc;
+    cin >> nc;
+
+    vector<int> ar(nc);
+    map<int, int> a;
+
+    for (int i = 0; i < nc; i++)
+    {
+        cin >> ar[i];
+        a[ar[i]]++, ar[i] = 1000 * a[ar[i]] + ar[i];
+    }
+    sort(ar.begin(), ar.end());
+    for (int i : ar)
+        cout << i % 1000 << " ";
+    cout << "\n";
+}
+
+/*
+Ebnesamit
+*/
+int32_t main()
+{
+    opscode();
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("Error.txt", "w", stderr);
+    // #endif
+
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+    {
+        samsolveit();
+    }
+
+    return 0;
+}
