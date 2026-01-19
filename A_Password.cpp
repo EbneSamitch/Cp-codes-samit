61cfd0a2bfd5e5dd9e260b9049d73267c5f0777d
Ebne Samit Chowdhury <ebnesm2@gmail.com>
2026-01-19 22:58:36 +0600

    jan19,26

---
+++ A_Password.cpp
@@ -0,0 +1,57 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ //
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define opscode()                     \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+#define dbg(p) cerr << #p << " " << p << "\n";
+#else
+#define dbg(p)
+#endif
+
+
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+typedef long long ll;
+typedef double dol;
+#define pi acos(-1)
+#define int long long
+
+void samsolveit()
+{
+    int nc;
+    cin >> nc;
+
+    vector<int> ar(nc);
+    for (int i = 0; i < nc; i++)
+    {
+        cin >> ar[i];
+    }
+    cout << (10 - nc) * (9 - nc) * 3 << "\n";
+}
+
+/*
+Ebnesamit
+*/
+int32_t main()
+{
+    opscode();
+
+//#ifndef ONLINE_JUDGE
+//    freopen("Error.txt", "w", stderr);
+//#endif
+
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+    {
+        samsolveit();
+    }
+
+    return 0;
+}
