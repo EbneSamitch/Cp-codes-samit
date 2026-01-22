b08409398f3054ee223fbe25826fe78aafb41025
Ebne Samit Chowdhury <ebnesm2@gmail.com>
2026-01-22 22:23:17 +0600

    jan22,26

---
+++ B_Was_it_Rated.cpp
@@ -0,0 +1,58 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ //
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define opscode()                     \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+#define dbg(p) cerr << #p << " " << p << "\n";
+#else
+#define dbg(p)
+#endif
+
+
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+typedef long long ll;
+typedef double dol;
+#define pi acos(-1)
+#define int long long
+
+void samsolveit()
+{
+    int nc;
+    cin >> nc;
+
+if(nc==15 || nc==20 || nc==21){
+    YES;
+}
+else{
+    NO;
+       
+}
+}
+
+/*
+Ebnesamit
+*/
+int32_t main()
+{
+    opscode();
+
+//#ifndef ONLINE_JUDGE
+//    freopen("Error.txt", "w", stderr);
+//#endif
+
+    // int tc;
+    // cin >> tc;
+
+    // while (tc--)
+    {
+        samsolveit();
+    }
+
+    return 0;
+}
