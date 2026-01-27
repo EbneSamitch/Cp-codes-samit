65d38444efef457de66163b01338724fc3265f7e
Ebne Samit Chowdhury <ebnesm2@gmail.com>
2026-01-27 22:32:24 +0600

    jan27,26

---
+++ A_Cowardly_Rooks.cpp
@@ -0,0 +1,66 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ //
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define opscode()                     \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+#define dbg(p) cerr << #p << " " << p << "\n";
+#else
+#define dbg(p)
+#endif
+
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+typedef long long ll;
+typedef double dol;
+#define pi acos(-1)
+#define int long long
+
+void samsolveit()
+{
+    int nc, mc;
+    cin >> nc >> mc;
+    map<int, int> mp1, mp2;
+
+    for (int i = 0; i < mc; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        mp1[x]++;
+        mp2[y]++;
+    }
+
+    if (mp1.size() < nc || mp2.size() < nc){
+        
+        YES;
+        return;
+    }
+
+        cout << "NO\n";
+}
+
+/*
+Ebnesamit
+*/
+int32_t main()
+{
+    opscode();
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("Error.txt", "w", stderr);
+    // #endif
+
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+    {
+        samsolveit();
+    }
+
+    return 0;
+}
